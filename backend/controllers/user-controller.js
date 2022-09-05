a74887d085aa33c24929aa51f2a6f0120e8b70f5
felipeazs <felipeazs@gmail.com>
2022-09-05 09:02:59 -0400

    Adding route functions comments. Passing body on post requests (body parser in server.js needed) Handling errors in /middleware import in server.js to catch all requests

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,6 +1,21 @@
+
+
+//@desc Register a new user
+//@route /api/users
+//@access public
 const registerUser = (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
     res.send('Register route')
 }
+
+//@desc Login user
+//@route /api/users/login
+//@access public
 const loginUser = (req, res) => {
     res.send('Register login')
 }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
+const { errorHandler } = require('./middleware/error-middleware')
 
 const app = express()
+
+//parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello' })
@@ -10,6 +15,9 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/api/users', require('./routes/user-routes'))
+
+//error handler
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
