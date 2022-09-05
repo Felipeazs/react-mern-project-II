27721284e0e44d851492c4c5a8a0740a49a8686e
felipeazs <felipeazs@gmail.com>
2022-09-05 15:08:57 -0400

    Backend: get single ticket, update and delete tickets functionality: ticket-controller Full CRUD ready

--- backend/controllers/ticket-controller.js
+++ backend/controllers/ticket-controller.js
@@ -19,6 +19,31 @@ const getTickets = asyncHandler(async (req, res) => {
     res.status(200).json(tickets)
 })
 
+//@desc GET user ticket
+//@route /api/ticket/:id
+//@access private
+const getTicket = asyncHandler(async (req, res) => {
+    //Information sended from the auth-middleware
+    const user = await User.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    res.status(200).json(ticket)
+})
+
 //@desc POST create ticket
 //@route /api/tickets
 //@access private
@@ -45,8 +70,64 @@ const createTicket = asyncHandler(async (req, res) => {
 
     res.status(201).json(ticket)
 })
+//@desc UPDATE user ticket
+//@route PUT /api/tickets/:id
+//@access private
+const updateTicket = asyncHandler(async (req, res) => {
+    //Information sended from the auth-middleware
+    const user = await User.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedTicket)
+})
+
+//@desc DELETE user ticket
+//@route /api/tickets/:id
+//@access private
+const deleteTicket = asyncHandler(async (req, res) => {
+    //Information sended from the auth-middleware
+    const user = await User.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized')
+    }
+
+    await Ticket.remove()
+
+    res.status(200).json({ success: true })
+})
 
 module.exports = {
     getTickets,
+    getTicket,
     createTicket,
+    updateTicket,
+    deleteTicket,
 }
--- backend/routes/ticket-routes.js
+++ backend/routes/ticket-routes.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-const { getTickets, createTicket } = require('../controllers/ticket-controller')
+const {
+    getTickets,
+    getTicket,
+    createTicket,
+    updateTicket,
+    deleteTicket,
+} = require('../controllers/ticket-controller')
 const protect = require('../middleware/auth-middleware.js')
 
 router.get('/', protect, getTickets)
+router.get('/:id', protect, getTicket)
 router.post('/', protect, createTicket)
+router.put('/:id', protect, updateTicket)
+router.delete('/:id', protect, deleteTicket)
 
 module.exports = router
