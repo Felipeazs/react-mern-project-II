af0aa530fe83357faf4f2105968618470ec36429
felipeazs <felipeazs@gmail.com>
2022-09-05 08:37:15 -0400

    Express: get requests

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 8000
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello' })
+})
+
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
+    console.log(`Server started on port: ${PORT}`)
 })
