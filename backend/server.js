3db653e71c2d937968244e4c4b5202668765f0ac
felipeazs <felipeazs@gmail.com>
2022-09-05 08:33:44 -0400

    Setting express server and dotenv config

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('server...');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
