ecd254ca809abf52a54a8248f19feef736e07cb2
felipeazs <felipeazs@gmail.com>
2022-09-06 07:15:15 -0400

    Frontend: New Ticket redux functionality (ticket-slice, ticket-service and store)

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import authReducer from '../features/auth/auth-slice'
+import ticketReducer from '../features/tickets/ticket-slice'
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        ticket: ticketReducer,
     },
 })
